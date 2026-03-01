41dc8712afa918bd6aaf4022708f17292b584d5a
Ayush9049 <ayushgawali11@gmail.com>
2026-03-01 20:55:10 +0530

    checkingGit

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Loading = ({ height = '100vh' }) => {
+const Loading = ({ height = '50vh' }) => {
     return (
         <div style={{ height }} className='flex items-center justify-center w-full'>
             <div className='w-10 h-10 rounded-full border-4 border-indigo-500 border-t-transparent animate-spin'>
