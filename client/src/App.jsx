c5f060e12ce078fdecb7b737e446aa8fead64bec
Sahil <sahilgulunjkar89@gmail.com>
2026-06-04 11:32:37 +0530

    fix: populate message sender details for real-time notifications and parse safely on client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,6 +9,7 @@ import Discover from './pages/Discover'
 import Profile from './pages/Profile'
 import CreatePost from './pages/CreatePost'
 import Layout from './pages/Layout'
+import Notification from './components/Notification'
 
 import { useUser, useAuth } from '@clerk/clerk-react'
 import Loading from './components/Loading'
@@ -26,40 +27,56 @@ export const App = () => {
   const pathnameRef = useRef(pathname)
 
   const dispatch = useDispatch();
-
+  
   useEffect(() => {
-    const fetchData = async () => {
-      if (user) {
-        const token = await getToken();
-        dispatch(fetchUser(token))
-        dispatch(fetchConnections(token))
+      const fetchData = async () => {
+        if(user) {
+          const token = await getToken();
+          dispatch(fetchUser(token))
+          dispatch(fetchConnections(token))
+        }
       }
-    }
-    fetchData();
-  }, [user, getToken, dispatch])
+      fetchData();
+  },[user, getToken, dispatch])
 
 
   useEffect(() => {
-    pathnameRef.curret = pathname;
+    pathnameRef.current = pathname;
   }, [pathname])
+
+  if (!isLoaded) {
+    return <Loading />
+  }
 
   useEffect(() => {
     if (user) {
       const eventSource = new EventSource(import.meta.env.VITE_BASE_URL + '/api/message/' + user.id)
-
+      
       eventSource.onmessage = (event) => {
-        const message = JSON.parse(event.data)
-
-        if (pathnameRef.current === ('/messages/') + message.from_user_id._id) {
-          dispatch(addMessage(message))
-        } else {
-          toast
+        try {
+          const data = JSON.parse(event.data)
+          if (data.type === "EDIT") {
+            dispatch(updateMessage(data.message))
+          } else if (data.type === "DELETE") {
+            dispatch(deleteMessage(data.id))
+          } else {
+            const fromUserId = typeof data.from_user_id === 'object' ? data.from_user_id._id : data.from_user_id
+            if (pathnameRef.current === '/messages/' + fromUserId) {
+              dispatch(addMessage(data))
+            } else {
+              toast.custom((t) => (
+                <Notification t={t} message={data} />
+              ), { position: "bottom-right" })
+            }
+          }
+        } catch (error) {
+          console.error("SSE parse error:", error)
         }
       }
       return () => {
         eventSource.close()
       }
-    }
+    } 
   }, [user, dispatch])
 
   return (
